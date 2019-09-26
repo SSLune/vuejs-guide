0c7500b1f1e1e371eefff86aa7e8328c14f856a5
Oriol Ferrer <oriolferrer@MacBook-Air-de-Oriol.local>
2019-09-26 12:14:45 +0800

    v1.1.0
    Components + communication events

--- main.js
+++ main.js
@@ -1,29 +1,64 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        product: 'Socks',
-        brand: 'Vue',
-        selectedVariant: 0,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-        variants: [
-            {
-                variantId: 1,
-                variantColor: 'blue',
-                variantImage: './assets/vmSocks-blue-onWhite.jpg',
-                variantQuantity: 50
-            },
-            {
-                variantId: 2,
-                variantColor: 'green',
-                variantImage: './assets/vmSocks-green-onWhite.jpg',
-                variantQuantity: 0
-            }
-        ],
-        cart: 0
+Vue.component('product', {
+    props: {
+        premium: {
+            type: Boolean,
+            required: true,
+        }
+    },
+    template:`
+        <div class="product">
+            <div class="product-image">
+                <img :src="image" alt="">
+            </div>
+            <div class="product-info">
+                <h1>{{ title }}</h1>
+                <p v-if="inStock">In stock</p>
+                <p v-else>Out of stock</p>
+                <p>Shipping: {{ shipping }}</p>
+                <ul>
+                    <li v-for="detail in details">{{ detail }}</li>
+                </ul>
+
+                <div v-for="(variant, index) in variants" 
+                     :key="variant.variantId"
+                     class="color-box"
+                     :style="{ backgroundColor: variant.variantColor }"
+                     @mouseover="updateVariant(index)"  >
+                </div>
+
+                <button v-on:click="addToCart" 
+                        :disabled="!inStock" 
+                        :class="{disabledButton: !inStock}">
+                        Add to Cart
+                </button>
+            </div>
+        </div>
+    `,
+    data(){
+        return  {
+            product: 'Socks',
+            brand: 'Vue',
+            selectedVariant: 0,
+            details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+            variants: [
+                {
+                    variantId: 1,
+                    variantColor: 'blue',
+                    variantImage: './assets/vmSocks-blue-onWhite.jpg',
+                    variantQuantity: 50
+                },
+                {
+                    variantId: 2,
+                    variantColor: 'green',
+                    variantImage: './assets/vmSocks-green-onWhite.jpg',
+                    variantQuantity: 0
+                }
+            ],
+        }
     },
     methods: {
         addToCart() {
-            this.cart += 1
+            this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId)
         },
         updateVariant(index) { 
             this.selectedVariant = index
@@ -38,6 +73,23 @@ var app = new Vue({
         },
         inStock() {
             return this.variants[this.selectedVariant].variantQuantity
+        },
+        shipping() {
+            return (this.premium) ? 'Free' : 2.99;
+        }
+    } 
+})
+
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        premium: false,
+        cart: []
+    },
+    methods: {
+        updateCart(id) {
+            this.cart.push(id)
         }
     }
 })
